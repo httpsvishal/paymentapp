8246dd3469e5ed3200f2b70520980969150c4b08
httpsvishal <121662780+httpsvishal@users.noreply.github.com>
2024-08-07 11:16:32 +0530

    Add files via upload

---
+++ src/components/third.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import confirm from "../animations/confirm.json";
+import Lottie from "lottie-react";
+
+const Confirmation = () => {
+    return(
+        
+        <>
+        <div className="text-center my-5">
+        <h1>
+            Your Booking has been confirmed!
+        </h1>
+        </div>
+        <div className="confirm flex justify-center items-center">
+            
+            <Lottie animationData={confirm} style={{width: "500px"}} loop={false} />
+            
+        </div>
+        </>
+    );
+}
+export default Confirmation;
